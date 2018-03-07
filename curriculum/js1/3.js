b65e4901cc44a39cc01ccf0281e8ab9163eaffd1
Alberto Lopez <alopez@garagescript.org>
2018-03-07 00:43:52 -0800

    3.js

--- curriculum/js1/3.js
+++ curriculum/js1/3.js
@@ -5,8 +5,11 @@
  * @returns {string}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, str='', i=0) => {
+  if(i === a){
+    return str;
+  };
+  return solution(a, str+'hello', i+1);
 };
 
 module.exports = {
