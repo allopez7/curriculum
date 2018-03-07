187f5084ff73e73e24afaa2eafa7bae663ca9775
Alberto Lopez <alopez@garagescript.org>
2018-03-07 15:24:54 -0800

    6.js

--- curriculum/js1/6.js
+++ curriculum/js1/6.js
@@ -5,8 +5,10 @@
  * @returns {number}
  */
 
-const solution = (a) => {
-  return 0;
+const solution = (num, sum=0, i=2) => {
+  if(i === num) return sum;
+  if(num%i === 0) sum += i;
+  return solution(num, sum, i+1);
 };
 
 module.exports = {
