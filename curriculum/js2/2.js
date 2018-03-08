7b66bb4c63c0bc052c815ce0047f0d19a2d160c1
Alberto Lopez <alopez@garagescript.org>
2018-03-07 20:27:14 -0800

    2_2.js js2

--- curriculum/js2/2.js
+++ curriculum/js2/2.js
@@ -9,6 +9,12 @@ const solution = (arr, newArr=[], i=0) => {
   if(i === arr.length){
     return newArr;
   }
+  if(arr[i]%2 != 0){
+    newArr.push(0);
+  }
+  if(arr[i]%2 === 0){
+    newArr.push(arr[i]);
+  }
   return solution(arr, newArr, i+1);
 };
 
