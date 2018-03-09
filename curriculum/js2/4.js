be0b5389413bf69e671e6c99716cb866f85329bf
Alberto Lopez <alopez@garagescript.org>
2018-03-08 16:06:34 -0800

    4.js

--- curriculum/js2/4.js
+++ curriculum/js2/4.js
@@ -5,7 +5,10 @@
  */
 
 const solution = (arr, i=0, sum=0) => {
-  if(i === arr.length || arr.length === 0){
+  if(arr.length === 0){
+    sum = '';
+  }
+  if(i === arr.length){
     return sum;
   }
   return solution(arr, i+1, sum + arr[i])
